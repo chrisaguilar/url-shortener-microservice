430824224dedb44b14fcda14caf6510e6bd36872
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-03-01 11:40:12 -0500

    MAJOR fix
    Instead of running each of these teeny tiny API projects
    as individual apps, I can just clone them into my freeCodeCamp
    folder and run them from in there.
    
    Now I only have three apps running. Really big decrease in RAM usage.

--- index.js
+++ index.js
@@ -11,47 +11,55 @@ const app = express();
 
 app.set('port', parseInt(process.env.PORT, 10));
 
-app.get('/api/all', asyncM(async (req, res) => {
-    const db = await sqlite;
+app.get(
+    '/api/all',
+    asyncM(async (req, res) => {
+        const db = await sqlite;
 
-    const rows = [...(await db.all('SELECT * FROM shortened_urls'))];
+        const rows = [...(await db.all('SELECT * FROM shortened_urls'))];
 
-    res.json(rows);
-}));
+        res.json(rows);
+    })
+);
 
-app.get('/api/:id', asyncM(async (req, res) => {
-    const db = await sqlite;
+app.get(
+    '/api/:id',
+    asyncM(async (req, res) => {
+        const db = await sqlite;
 
-    if (!req.params.id) throw new Error('No shortened URL specified!');
+        if (!req.params.id) throw new Error('No shortened URL specified!');
 
-    const { id } = req.params;
-    const row = await db.get('SELECT url FROM shortened_urls WHERE id = ?;', id);
+        const { id } = req.params;
+        const row = await db.get('SELECT url FROM shortened_urls WHERE id = ?;', id);
 
-    if (!row) throw new Error(`https://chrismaguilar.com/url-shortener/api/${id} is not a valid shortened URL!`)
+        if (!row) throw new Error(`https://chrismaguilar.com/url-shortener/api/${id} is not a valid shortened URL!`);
 
-    res.redirect(row.url);
-}));
+        res.redirect(row.url);
+    })
+);
 
-app.post('/api', bodyParser.json(), asyncM(async (req, res) => {
-    const db = await sqlite;
+app.post(
+    '/api',
+    bodyParser.json(),
+    asyncM(async (req, res) => {
+        const db = await sqlite;
 
-    const id = shortid.generate();
-    const { url } = req.body;
+        const id = shortid.generate();
+        const { url } = req.body;
 
-    if (!url) throw new Error('No URL specified!');
+        if (!url) throw new Error('No URL specified!');
 
-    await db.run('INSERT INTO shortened_urls (id, url) VALUES (?, ?);', id, url);
+        await db.run('INSERT INTO shortened_urls (id, url) VALUES (?, ?);', id, url);
 
-    res.json({ id, url });
-}));
+        res.json({ id, url });
+    })
+);
 
 app.get('*', (req, res) => res.sendFile(join(__dirname, 'index.html')));
 
 app.use((err, req, res, next) => res.send(err.message));
 
-app.listen(app.get('port'), async () => {
-    console.log(`/url-shortener listening on port ${app.get('port')}`);
-
+async function migrate() {
     try {
         const db = await sqlite;
         db.migrate({ migrationsPath: join(__dirname, 'migrations') });
@@ -59,4 +67,11 @@ app.listen(app.get('port'), async () => {
         console.errror(e);
         process.exit(1);
     }
-})
+}
+
+if (process.env.SINGLE) {
+    migrate();
+    app.listen(app.get('port'), () => console.log(`/url-shortener listening on ${app.get('port')}`));
+} else {
+    module.exports = app;
+}
